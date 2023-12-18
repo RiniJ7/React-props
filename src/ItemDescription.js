75694da3ffaa55ce34d6cde4aa3e79e283120c07
rinij <rinimani7@gmail.com>
2023-12-18 15:19:02 -0700

    used props in class based components

--- src/ItemDescription.js
+++ src/ItemDescription.js
@@ -1,14 +1,47 @@
-import React from 'react'
+import React , { Component } from "react";
 
-const ItemDescription = ({name, description}) => {
-  return (
-    <div>
-    <p>{name}</p>
-    <p>
-    <i>{description}</i>
-    </p>
-    </div>
-  )
-}
+// props using function based components
+// const ItemDescription = ({name, description}) => {
+//   return (
+//     <div>
+//     <p>{name}</p>
+//     <p>
+//     <i>{description}</i>
+//     </p>
+//     </div>
+//   )
+// }
 
-export default ItemDescription
+// export default ItemDescription
+
+// props using class based components although this is outdated
+
+// export default class ItemDescription extends Component {
+//   render() {
+//     return (
+//     <div>
+//       <p>{this.props.name}</p>
+//       <p>
+//         <i>{this.props.description}</i>
+//       </p>
+//     </div>
+//     );
+//   }
+// }
+
+//Another way of cleaning up your code
+
+export default class ItemDescription extends Component {
+    render() {
+        // going inside of this.props and pulling up name and description
+        const {name, description} = this.props;
+      return (
+      <div>
+        <p>{name}</p>
+        <p>
+          <i>{description}</i>
+        </p>
+      </div>
+      );
+    }
+  }
